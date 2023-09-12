17a2c8a409295899a05c3fdbbe2de4d11c336881
unknown <riswan20102001@gmail.com>
2023-09-12 10:12:45 +0530

    First commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+  
+namespace App\Http\Controllers;
+  
+use Illuminate\Http\Request;
+use App\Models\User;
+  
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('createUser');
+    }
+    
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+     public function store()
+     {
+         $this->validate(request(), [
+             'name' => 'required|regex:/^[A-Z]+$/i',
+             'gender' => 'required',
+             'phone' => 'required|numeric|max:10',
+             'bday' => 'required',
+             'email' => 'required|email|unique:users,email',
+             'password' => 'required|min:4|max:10|'
+         ]);
+         
+         $user = new User;
+         
+         $user->name = request('name'); 
+         $user->gender = request('gender');
+         $user->phone = request('phone');
+         $user->bday = request('bday');
+         $user->email = request('email');
+         $user->password = request('password');
+         $user->save();
+        return back()->with('success', 'User created successfully.');
+    }
+}
